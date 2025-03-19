febffe1c8860a28b4362821b54899778016c6fcc
Michel-Gomes33 <michel.lrgomes@outlook.com>
2025-03-19 19:30:46 -0400

    css

---
+++ React-Portfolio/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer>
+            <a href="https://github.com/Michel-Gomes33" target="_blank" >Github</a>
+            <a href="https://www.linkedin.com/in/michel-gomes-a04455145/" target="_blank" >LinkedIn</a>
+            <a href="https://github.com/https://www.notion.so/Writing-Portfolio-d647a2d38475449c859b6263c9246409-Gomes33" target="_blank" >Notion</a>
+        </footer>
+    );
+
+};
+
+export default Footer;
