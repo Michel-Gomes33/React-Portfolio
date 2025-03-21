580437aa9e3285de17117c62b17ee07daa5f0891
Michel-Gomes33 <michel.lrgomes@outlook.com>
2025-03-20 20:38:22 -0400

    v2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import Navigation from "./Navigation";
+import { NavLink } from "react-router-dom";
 import "../styles/header.css";
 
 const Header = () => {
@@ -6,9 +7,9 @@ const Header = () => {
         <header className="header">
             <h1>Michel Gomes</h1>
             <Navigation />
-            <a className="cta" href="#"><button><NavLink to="Contact">Contact</NavLink></button></a>
+            <NavLink to="/contact" className="cta-button">Contact</NavLink>
         </header>
     );
 };
 
-export default Header ;
+export default Header;
